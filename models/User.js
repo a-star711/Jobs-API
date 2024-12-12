9b94652a65c661d96bb5712508f89381750c33ea
a-star711 <a.krasimirov35@gmail.com>
2024-12-12 21:39:19 +0200

    changed password hashing to be done with mongoose middleware

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name:{
@@ -19,6 +20,12 @@ const UserSchema = new mongoose.Schema({
     required:[true, 'Please provide password'],
     minLength:6,
   },
+})
+
+UserSchema.pre('save', async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+
 })
 
 module.exports = mongoose.model('User', UserSchema)
