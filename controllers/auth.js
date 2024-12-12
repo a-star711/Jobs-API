ad880ecc303eb5db4b676b6287be12b61e922627
a-star711 <a.krasimirov35@gmail.com>
2024-12-12 23:08:08 +0200

    Generate Token with instance method & amend secretkey

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,29 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
-
 
 const register = async (req, res) => {
 
-  const user = await User.create({ ...req.body })
 
-  res.status(StatusCodes.CREATED).json({user})
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+
+
+  res.status(StatusCodes.CREATED).json({user:{name:user.name}, token })
 }
 
 const login = async (req, res) => {
+  //  const {email, password} = req.body 
+
+  //  if(!username || !password) {
+  //   throw new BadRequestError('Please provide email and password')
+  //  }
+
+  //  const id = new Date().getDate()
+
+  //  const token = jwt.sign({email, password}, process.env.JWT_SECRET, {expiresIn:'30d'})
+
+
   res.send('login user')
 }
 
