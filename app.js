80760118b498b5b752de86a6b8f1f8f32e21edc4
a-star711 <a.krasimirov35@gmail.com>
2024-12-13 16:20:45 +0200

    Verify user token with auth middleware

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 
 //connect DB
 const connectDB = require('./db/connect')
-
-// routers
+const authMiddleware = require('./middleware/authentication');
+// router
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authMiddleware, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ middleware/authentication.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const user = require('../models/User')
+const { UnauthenticatedError} = require('../errors')
+
+const authenticationMiddleware = async (req,res,next) => {
+  const authHeader = req.headers.authorization;
+
+  console.log('Authorization Header:', authHeader); // Debugging header
+
+  if(!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication ivalid')
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+    //attach the user to the job routes
+
+    req.user = { userId: payload.userId, name: payload.name };
+    console.log('User Attached to Request:', req.user); // Debugging attached user
+
+    next()
+
+    
+  } catch (error) {
+    console.error('JWT Verification Error:', error.message); // Debugging verification error
+    throw new UnauthenticatedError('Not authorized to use this route')
+    
+  }
+
+}
+
+module.exports = authenticationMiddleware
