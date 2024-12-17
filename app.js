b005f2e99519cb8115929e1e87a4f3697b328d24
a-star711 <a.krasimirov35@gmail.com>
2024-12-17 11:56:49 +0200

    Add security packages before deployment

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
-const express = require('express');
+
+//  security packages 
+
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit')
+
+const express = require('express')
 const app = express();
 
 //connect DB
@@ -14,7 +22,16 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(rateLimit({
+  windowsMs: 15*60*1000,  // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowsMs
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 // extra packages
 
 // routes
